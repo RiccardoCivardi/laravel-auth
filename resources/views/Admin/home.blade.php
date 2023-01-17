bc112ee16cf527a25de3f8a7fafd52c8bc4cbbd2
RiccardoCivardi <riccardo.civardi@icloud.com>
2023-01-17 21:07:33 +0100

    [add] route details and protected link experimentation

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -2,22 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <h1>Questa è la dashboard di {{Auth::user()->name}}</h1>
+    <h5>L'email è {{Auth::user()->email}}</h5>
+    <h5>L'account è stato creato il: {{Auth::user()->created_at}}</h5>
 </div>
 @endsection
